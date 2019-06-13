7670df13294bb0b4f3dffbe3579743afcbcf4ab0
kevin lang <langkevina@gmail.com>
2019-06-13 02:02:18 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+  def initialize
+    @knowledge = []
+  end
+  def learn(learn)
+    knowledge << learn
+  end
 
+  def knowledge
+    @knowledge
+  end
+
+  def teach
+    
+  end
 end
