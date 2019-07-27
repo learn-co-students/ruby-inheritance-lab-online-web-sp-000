7aaa3404b224678960506ebb7818c3c29cc5218d
github username <github email address>
2019-07-27 10:41:40 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require 'pry'
 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = [] 
+    end
+
+    def learn(str)
+        @knowledge.push(str) 
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
