3d627c0b28f95d8a760d578fe3bde85d02777b02
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-24 17:52:33 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,13 @@
-class Teacher
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-
+  
+  def teach
+    KNOWLEDGE.sample
+  end
   
 end
+
+# rspec spec/student_spec.rb
+# rspec spec/teacher_spec.rb
+# rspec spec/user_spec.rb
---
+++ lib/user.rb
@@ -0,0 +1,12 @@
+
+class User
+  @@all = []
+  attr_accessor :first_name, :last_name
+  
+  
+  
+end
+
+# rspec spec/student_spec.rb
+# rspec spec/teacher_spec.rb
+# rspec spec/user_spec.rb
