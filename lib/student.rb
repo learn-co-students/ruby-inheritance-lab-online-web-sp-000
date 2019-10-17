79f68010f9128bba4f832b91933781d5f0d23907
Richard Lee <richlee81192@gmail.com>
2019-10-17 23:11:42 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+
+class Student < User
+attr_accessor :first_name, :last_name
+  def first_name
+    @first_name = first_name
+  end
+  
+  def last_name
+    @last_name = last_name
+  end 
 
 end
