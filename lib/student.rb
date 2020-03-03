72417b4049359c4552a939dae1313fce63e8be35
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-03-03 02:03:29 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,19 @@ class Student < User
 
   attr_accessor :knowledge
 
+  @@all = []
+
   def initialize
+    @@all << self
     @knowledge = []
   end
 
+  def self.all
+    @@all
+  end
+
   def learn(knowledge)
-    @knowledge = knowledge
+    @knowledge << knowledge
   end
 
   def knowledge
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,6 +3,13 @@ class Teacher < User
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   def teach
-     KNOWLEDGE.each {|a| return a}
+     KNOWLEDGE.sample
+  end
+  def teaches_class
+    some_knowledge = self.teach
+    Student.all.each do |student|
+       student.learn(some_knowledge)
+       puts "Thank you #{@first_name}, #{student.first_name} just learned: #{student.knowledge.sample}"
+     end
   end
 end
