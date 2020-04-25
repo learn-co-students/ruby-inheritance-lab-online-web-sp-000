c1b4bdae42047df3b3a5e4a0459f72543827fa49
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-25 20:44:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
 class Student 
-
+  attr_accessor :first_name, :last_name, :knowledge
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  
+  def knowledge
+    @knowledge
+  end
+  
 end
+
