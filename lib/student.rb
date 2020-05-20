405f52c5d0994a5f118abe560c02e9b447aad1e0
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-20 15:10:13 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+  attr_reader :knowledge
+  def initialize
+    @knowledge = Array.new
+  end
+
+  def learn(knowledge_string)
+    @knowledge << knowledge_string
+  end
 
 end
