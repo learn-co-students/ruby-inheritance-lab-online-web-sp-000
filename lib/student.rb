99f9c60151e742ff0db66de1de75f94a40535598
Damian Malysza <damian.malysza@gmail.com>
2020-11-03 22:48:22 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative '../lib/user.rb'
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(lesson)
+    @knowledge << lesson
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
