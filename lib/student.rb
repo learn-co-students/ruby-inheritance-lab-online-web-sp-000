47d9ee48045e561e825ea73b19a81b283c744c1e
Mason Phillips <mjphillips2000@gmail.com>
2020-11-05 07:13:51 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
-
+require_relative "./user.rb"
+class Student < User
+  
+  attr_accessor :first_name, :last_name, :knowledge
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def first_name
+    @first_name
+  end
+  
+  def last_name
+    @last_name
+  end
+  
+  def learn(string)
+    @knowledge << string
+  end
 end
