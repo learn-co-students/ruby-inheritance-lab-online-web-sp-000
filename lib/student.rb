c984f58229b682cabe775fed18d7e21ef2acb3ac
Mike Barberry <mbarberry@Mikes-MacBook-Pro.local>
2021-01-23 19:47:04 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require 'user.rb'
 
+class Student < User
+    attr_accessor :knowledge 
+
+    def initialize
+        @knowledge = []
+    end 
+
+    def knowledge
+        @knowledge
+    end 
+
+    def learn(info)
+        @knowledge << info
+    end 
 end
