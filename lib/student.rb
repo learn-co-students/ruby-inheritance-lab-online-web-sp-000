81a9a9dcec1a939eacfe29342be0455154fc9d84
Jouse then <jthen93@gmail.com>
2021-03-02 06:17:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
-
+class Student < User
+  attr_accessor :knowledge
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  
+  def knowledge
+    return @knowledge
+  end
+  
+  def initialize()
+    @knowledge = []
+  end
+  
 end
