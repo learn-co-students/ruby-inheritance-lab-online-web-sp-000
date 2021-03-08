15ab74246bd43c12d1a3b794294ce73f988199fe
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-08 20:22:39 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,13 +2,17 @@ require 'pry'
 
 class Student < User 
 
+ KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  
   def initialize 
     knowledge = []
   end 
   
-  def learn
-    KNOWLEDGE.map do |known|
-      knowledge << known 
+  def learn(first_name, last_name)
+    Student.new
+      knowledge << known
   end
 end 
 end 
+
+#expect(sophie.instance_variable_get(:@knowledge)).to eq([]) 
