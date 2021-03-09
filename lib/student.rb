2503b6b9d2f724e80681da0dafea8e3971024b48
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-09 01:48:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,13 +5,16 @@ class Student < User
  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   
   def initialize 
-    knowledge = []
+    @knowledge = []
   end 
   
-  def learn(first_name, last_name)
-    Student.new
-      knowledge << known
+  def learn(string_of_knowledge)
+    @knowledge << string_of_knowledge
   end
+  
+  
+  def knowledge 
+    @knowledge 
 end 
 end 
 
