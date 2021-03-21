9dfa1f12e40226eff618ef60c113907a695ad18a
Caroline <carolinechavezt@outlook.com>
2021-03-20 21:23:48 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+    attr_accessor :first_name, :last_name
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(argument)
+        @knowledge << argument
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
