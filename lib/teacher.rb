5851f721328a26a0e11a97683eb9b6602777bb6c
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-24 22:52:20 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,15 @@
-class Teacher
+
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+@@all = []
+  attr_accessor :first_name, :last_name
+  def initialize(*)
 
-  
+    super
+  end
+
+def teach
+  KNOWLEDGE.sample
+  end
 end
